64e28fad67ac30cd9769fcf6178e5d804e1ecc99
jugyo <jugyo.org@gmail.com>
2009-05-23 23:29:51 +0900

    Created specs.

--- spec/g_spec.rb
+++ spec/g_spec.rb
@@ -1,14 +1,25 @@
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'g'
 
-g 'foo', 'bar'
-g 1
-g self
-g %w(foo bar)
-g ({:a=>"aaaaaaaaaaaaaaaaaa",
-   :b=>"bbbbbbbbbbbbbbbbbbb",
-   :c=>
-    ["cccccccccccccccc",
-     "CCCCCCCCCCCCCCCCC",
-     "c c c c c ",
-     "C C C C C C C C C "]})
+describe 'g' do
+  it 'calls with a arg' do
+    Kernel::GROWL.should_receive(:notify).with("Kernel.g", $0, 'foo'.pretty_inspect)
+    g 'foo'
+  end
+
+  it 'calls with args' do
+    Kernel::GROWL.should_receive(:notify).twice
+    g 'foo', 'bar'
+  end
+
+  it 'calls without args' do
+    Kernel::GROWL.should_receive(:notify).with("Kernel.g", $0, nil.pretty_inspect)
+    g
+  end
+
+  it 'should return original args' do
+    g('foo').should         == 'foo'
+    g('foo', 'bar').should  == ['foo', 'bar']
+    g().should              == nil
+  end
+end
