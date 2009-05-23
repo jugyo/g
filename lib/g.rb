90c5972ac4ca782f3ffbb3ab1ea925a7a5e1343c
jugyo <jugyo.org@gmail.com>
2009-05-23 23:05:49 +0900

    Changed to return original args.

--- lib/g.rb
+++ lib/g.rb
@@ -6,6 +6,12 @@ module Kernel
   GROWL = Growl.new "localhost", $0, ["Kernel.g"]
   def g(*args)
     args.each { |i| GROWL.notify "Kernel.g", $0, i.pretty_inspect }
-    nil
+    if args.empty?
+      nil
+    elsif args.size == 1
+      args.first
+    else
+      args
+    end
   end
 end
