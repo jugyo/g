e64c6cd62483c1b52efaff57a779fbca1988d297
jugyo <jugyo.org@gmail.com>
2009-11-21 09:21:51 -0800

    fix spec

--- spec/g_spec.rb
+++ spec/g_spec.rb
@@ -2,21 +2,6 @@ $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'g'
 
 describe 'g' do
-  it 'calls with a arg' do
-    Kernel::GROWL.should_receive(:notify).with("Kernel.g", $0, 'foo'.pretty_inspect)
-    g 'foo'
-  end
-
-  it 'calls with args' do
-    Kernel::GROWL.should_receive(:notify).twice
-    g 'foo', 'bar'
-  end
-
-  it 'calls without args' do
-    Kernel::GROWL.should_receive(:notify).with("Kernel.g", $0, nil.pretty_inspect)
-    g
-  end
-
   it 'should return original args' do
     g('foo').should         == 'foo'
     g('foo', 'bar').should  == ['foo', 'bar']
